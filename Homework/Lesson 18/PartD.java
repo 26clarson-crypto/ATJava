edd0adec18e9f08a33df7e2a871d7a4a81c6a691
26clarson-crypto <26clarson@derryfield.org>
2026-01-21 22:08:50 -0500

    Update PartD.java

--- Homework/Lesson 18/PartD.java
+++ Homework/Lesson 18/PartD.java
@@ -1,11 +1,7 @@
-/* Write a Java program that prompts the user for a series of integer values:
-Store the values in an ArrayList using the Integer wrapper class
-User enters 'q' to quit inputting data
-After you've acquired all the data, go through the list of numbers and remove every odd number from the ArrayList
-Print out the modified list, one number per line
-
-Test your code using the following sequence of integer values,
-1, 1, 2, 3, 4, 5, 5, 6, 7, 8
+/* Chloe Larson
+Part D
+Lesson 18
+Used the internet for help with removing odd numbers from the list and not ending the loop if invalid input is given.
  */
 
 import java.util.ArrayList;
