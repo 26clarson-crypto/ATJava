4a452420b72921702d20ab5343ed0553a35a1b4f
26clarson-crypto <26clarson@derryfield.org>
2026-02-10 18:48:57 -0500

    Lesson 22

---
+++ Homework/Lesson 22/PartA.java
@@ -0,0 +1,42 @@
+/* Chloe Larson
+Part A
+ */
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class PartA {
+    public static void main(String[] args) {
+        Scanner inputScanner = new Scanner(System.in);
+        String filename;
+
+        while (true) {
+            System.out.print("Please enter a filename (or # to quit): ");
+            filename = inputScanner.nextLine();
+
+            if (filename.equals("#")) {
+                System.out.println("Goodbye!");
+                break;
+                // end code if user enters #
+            }
+
+            try {
+                File file = new File(filename);
+                Scanner fileScanner = new Scanner(file);
+
+                while (fileScanner.hasNext()) {
+                    String word = fileScanner.next();
+                    System.out.println(word);
+                }
+
+                fileScanner.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("Error: File not found. Please try again.");
+                // error message
+            }
+        }
+
+        inputScanner.close();
+    }
+}
