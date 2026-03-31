cd4908ce26e1c33ea5f071bc56d0f0cc3afd8b20
26clarson-crypto <26clarson@derryfield.org>
2026-03-30 20:59:54 -0400

    Part A HW

--- ClassWork/March30/GearedBicycle.java
+++ ClassWork/March30/GearedBicycle.java
@@ -1,18 +1,24 @@
+/* Chloe Larson
+Part A
+HW and Class
+Used class discussion and some internet research for override techniques */
+
+
 public class GearedBicycle extends Bicycle {
     private int gear = 1; // default gear is 1
     final private int MAX_GEAR = 6;
     final private int GEARED_MAX_SPEED = 50;
-    
+
     // make explicit constructor
     public GearedBicycle() {
         // call the parent constructor
-         super();
-        // initilize the gear to gear #1
-       gear = 1;
+        super();
+        // initialize the gear to gear #1
+        gear = 1;
     }
 
-    public int getGear() { 
-        return gear; 
+    public int getGear() {
+        return gear;
     }
 
     public void setGear(int gear) {
@@ -21,15 +27,14 @@ public class GearedBicycle extends Bicycle {
         }
     }
 
-    public void shiftUp () {
+    public void shiftUp() {
         gear = gear + 1;
         if (gear > MAX_GEAR) {
             gear = MAX_GEAR;
         }
-
     }
 
-    public void shiftDown () {
+    public void shiftDown() {
         gear = gear - 1;
         if (gear < 1) {
             gear = 1;
@@ -37,38 +42,47 @@ public class GearedBicycle extends Bicycle {
     }
 
     // override the pedal method from the bicycle class
-    public void pedal () {
-        // dont increase by one but the gear number
+    public void pedal() {
+        // don't increase by one but the gear number
         int newSpeed = getSpeed() + gear;
 
-    // did we exceed max speed? 
+        // did we exceed max speed?
         if (newSpeed > GEARED_MAX_SPEED) {
             newSpeed = GEARED_MAX_SPEED;
         }
-        // update speed 
-       this.setSpeed(newSpeed);
+        // update speed
+        this.setSpeed(newSpeed);
     }
 
-    // override brake method 
-
+    // override brake method
+    // used internet mostly here..
+    // A geared bicycle has better brakes: slows down TWICE as fast
+    // as a conventional bicycle (which reduces speed by 1 per call).
+    @Override
+    public void brake() {
+        int newSpeed = getSpeed() - 2;   // 2x braking power
+        if (newSpeed < 0) {
+            newSpeed = 0;                // speed can never go negative
+        }
+        this.setSpeed(newSpeed);
+    }
 
     // override the display method
     public void display() {
-        // for a geared bicycle: display the current speed to the screen then the current gear
         System.out.println("Speed: " + getSpeed());
-        System.out.println ("Current gear = " + gear);
+        System.out.println("Current gear = " + gear);
     }
 
     public static void main(String[] args) {
         GearedBicycle b = new GearedBicycle();
         b.setGear(2);
         b.pedal();
-        b.display(); // Should print "Speed: 2 Gear: 2"
+        b.display(); // Should print "Speed: 3 Gear: 2"  (starts at 1, +2)
 
         GearedBicycle c = new GearedBicycle();
         c.shiftUp();
         c.pedal();
         c.pedal();
-        c.display(); // Should print "Speed: 6 Gear: 2"
+        c.display(); // Should print "Speed: 7 Gear: 2"  (starts at 1, +2+2+2)
     }
 }
